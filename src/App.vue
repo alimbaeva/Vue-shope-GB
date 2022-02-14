609c30ac63f9cde7b4fb35f1501280f24e18f67d
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-14 19:24:01 +0300

    fix: delete files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <h2>Hello, {{name}}</h2>
-  <input type='text' v-model='name' />
+  <div id='app'>
+    <header>
+      <span class='logo'>SHOP</span>
+      <input type='text' class='search' />
+      <button class='cart-button'>Корзина</button>
+    </header>
+    <main>
+      <section class='goods'>
+        <div class='item' v-for='item in filteredGoods'>
+          <h2>{{item.product_name}}</h2>
+          <p>{{item.price}}</P>
+          <button name="add-to-basket" v-on:click.prevent="addItem(item)">Add to basket</button>
+        </div>
+        <div class='goods-empty' v-if="filteredGoods.length === 0">
+          нету товаров
+        </div>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -9,7 +26,14 @@
       return{
         name: 'John'
       }
+    },
+
+    computed: {
+      filteredGoods(){
+
+      }
     }
+
   }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
 
 
-const routes = [
+// // const routes = [
 
-]
+// // ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
 
-export default router
+// export default router
